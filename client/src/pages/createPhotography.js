722990104fee87c8cf9b4362183619a394a4dcd9
AntoniyaNacheva <99907501+AntoniyaNacheva@users.noreply.github.com>
2023-03-27 17:24:42 +0300

    Create photography page

--- client/src/pages/createPhotography.js
+++ client/src/pages/createPhotography.js
@@ -1,7 +1,72 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { getUserId } from '../hooks/getUserId';
+import { useNavigate } from 'react-router-dom';
+
 export const CreatePhotography = () => {
+	const userID = getUserId();
+
+	const [photography, setPhotography] = useState({
+		name: '',
+		imageUrl: '',
+		destination: '',
+		subject: '',
+		style: '',
+		typeOfCamera: '',
+		userOwner: userID
+	});
+
+	const navigate = useNavigate();
+
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		setPhotography({ ...photography, [name]: value });
+	};
+
+	const onSubmit = async (event) => {
+		event.preventDefault();
+		try {
+			await axios.post('http://localhost:3001/photographs', photography);
+			alert('Photography created!');
+			navigate('/');
+
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
 	return (
-		<div>
-			CreatePhotography
+		<div className="create-photography">
+			<h2> Create Photography </h2>
+			<form onSubmit={onSubmit}>
+				<label htmlFor="name"> Name </label>
+				<input type="text" id="name" name="name" onChange={handleChange} />
+				<label htmlFor="imageUrl"> Image URL </label>
+				<input type="text" id="imageUrl" name="imageUrl" onChange={handleChange} />
+				<label htmlFor="destination"> Destination </label>
+				<input type="text" id="destination" name="destination" onChange={handleChange} />
+				<label htmlFor="subject"> Subject </label>
+				<input type="text" id="subject" name="subject" onChange={handleChange} />
+				<label htmlFor="style"> Style </label>
+				<input type="text" id="style" name="style" onChange={handleChange} />
+				<label htmlFor="typeOfCamera"> Type of camera </label>
+				<input type="text" id="typeOfCamera" name="typeOfCamera" onChange={handleChange} />
+				<button type="submit"> Create </button>
+			</form>
 		</div>
 	);
 };
+
+// imageUrl
+// destination
+// subject
+// style
+// typeOfCamera
+
+
+// Miracle
+// https://dailywildlifephoto.nathab.com/photography-guide/wp-content/uploads/2015/11/Bryce-Canyon-manzanita-bush.jpg
+// USA
+// Bryce Canyon
+// Landscape Photography
+// DSLR camera
