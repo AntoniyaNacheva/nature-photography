589c2b5032fbad3ee547eb724b8e6214b199bcea
AntoniyaNacheva <99907501+AntoniyaNacheva@users.noreply.github.com>
2023-04-07 18:44:56 +0300

    Footer

--- client/src/components/EditPhotography/EditPhotography.js
+++ client/src/components/EditPhotography/EditPhotography.js
@@ -30,26 +30,28 @@ const EditPhotography = () => {
     };
 
     return (
-        <section id="edit-page" className="auth">
-            <form id="edit" onSubmit={onSubmit}>
-                <div className="container">
-                    <h1>Edit Photography</h1>
-                    <label htmlFor="name">Name:</label>
-                    <input type="text" id="name" name="name" defaultValue={currentPhotography.name} />
-                    <label htmlFor="destination">Destination:</label>
-                    <input type="text" id="destination" name="destination" defaultValue={currentPhotography.destination} />
-                    <label htmlFor="subject">Subject:</label>
-                    <input
-                        type="text"
-                        id="subject"
-                        name="subject"
-                        defaultValue={currentPhotography.subject}
-                    />
-                    <label htmlFor="photography-img">Image:</label>
-                    <input type="text" id="imageUrl" name="imageUrl" defaultValue={currentPhotography.imageUrl} />
-                    <input className="btn submit" type="submit" defaultValue="Edit Photography" />
-                </div>
-            </form>
+        <section className='loginContainer'>
+            <div className="register-page">
+                <form id="edit" onSubmit={onSubmit}>
+                    <div className="container">
+                        <h1>Edit Photography</h1>
+                        <label htmlFor="name">Name:</label>
+                        <input type="text" id="name" name="name" defaultValue={currentPhotography.name} />
+                        <label htmlFor="destination">Destination:</label>
+                        <input type="text" id="destination" name="destination" defaultValue={currentPhotography.destination} />
+                        <label htmlFor="subject">Subject:</label>
+                        <input
+                            type="text"
+                            id="subject"
+                            name="subject"
+                            defaultValue={currentPhotography.subject}
+                        />
+                        <label htmlFor="photography-img">Image:</label>
+                        <input type="text" id="imageUrl" name="imageUrl" defaultValue={currentPhotography.imageUrl} />
+                        <input className="btn submit" type="submit" defaultValue="Edit Photography" />
+                    </div>
+                </form>
+            </div>
         </section>
     );
 };
--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,5 +1,28 @@
 const Footer = () => {
-    return (<footer className="footer"></footer>);
+	return (
+		<footer className="site-footer">
+			<div className="footer">
+				<div className="container">
+					<div className="row">
+						<div className="col-sm-12 col-md-6">
+							<h6>About us</h6>
+							<p className="text-justify">naturephotography.com is inspired by the wonders of nature in the most remote and and pristine corners of the world.</p>
+						</div>
+					</div>
+				</div>
+				<div className="container">
+					<div className="row">
+						<div className="col-md-8 col-sm-6 col-xs-12">
+							<p className="copyright-text">Copyright &copy; 2023 All Rights Reserved by Antoniya Nacheva
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
+
+
